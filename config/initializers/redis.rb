cc86607c5cfe4f833df1138fd6e01cbcc340d3c6
Stephen Doxsee <stephen@doxsee.org>
2012-05-08 17:52:03 -0400

    Add highscore_lb in redis.rb initializer

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,4 @@
 uri = URI.parse(ENV["REDISTOGO_URL"])
 REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+redis_options = {:redis_connection => REDIS}
+highscore_lb = Leaderboard.new('highscores', Leaderboard::DEFAULT_OPTIONS, redis_options)
